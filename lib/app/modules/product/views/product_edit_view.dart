a6ec004b1e1eba41e21993d4565423f677b45625
Lógica Booleana <logica.booleana.2019@gmail.com>
2021-12-11 07:28:22 -0300

    commit

--- lib/app/modules/product/views/product_edit_view.dart
+++ lib/app/modules/product/views/product_edit_view.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:animate_do/animate_do.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:producto/app/models/catalogo_model.dart';
 import 'package:producto/app/modules/mainScreen/controllers/welcome_controller.dart';
 import 'package:producto/app/modules/product/controllers/product_edit_controller.dart';
+import 'package:producto/app/services/database.dart';
 import 'package:producto/app/utils/widgets_utils_app.dart';
 import 'package:producto/app/utils/widgets_utils_app.dart' as utilsWidget;
+import 'package:search_page/search_page.dart';
 
 class ProductEdit extends StatelessWidget {
   ProductEdit({Key? key}) : super(key: key);
@@ -54,9 +62,6 @@ class ProductEdit extends StatelessWidget {
                   color:
                       Theme.of(contextPrincipal).textTheme.bodyText1!.color)),
       actions: <Widget>[
-        IconButton(
-            icon: controller.getSaveIndicator ? Container() : Icon(Icons.delete),
-            onPressed: controller.delete),
         IconButton(
             icon: controller.getSaveIndicator ? Container() : Icon(Icons.check),
             onPressed: controller.save),
@@ -221,31 +226,38 @@ class ProductEdit extends StatelessWidget {
                   : Container(),
 
           /* OPCIONES PARA DESARROLLADOR - ELIMINAR ESTE CÓDIGO PARA PRODUCCION */
-          SizedBox(height: 20.0),
-          Row(
-            children: [
-              Expanded(
-                  child: Divider(
-                height: 2.0,
-                endIndent: 12.0,
-                indent: 12.0,
-              )),
-              Text("Opciones para desarrollador"),
-              Expanded(
-                  child: Divider(height: 1.0, endIndent: 12.0, indent: 12.0))
-            ],
-          ),
-          //SizedBox(height: 20.0),
-          //buttonAddFavorite(context: builderContext),
-          SizedBox(height: !controller.getSaveIndicator ? 20.0 : 0.0),
-          controller.getSaveIndicator
+          controller.getNewProduct
               ? Container()
-              : buttonEditProductoOPTDeveloper(),
-          SizedBox(height: 20.0),
-          controller.getSaveIndicator
-              ? Container()
-              : buttonDeleteProductoOPTDeveloper(),
-          SizedBox(height: 50.0),
+              : Column(
+                  children: [
+                    SizedBox(height: 20.0),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: Divider(
+                          height: 2.0,
+                          endIndent: 12.0,
+                          indent: 12.0,
+                        )),
+                        Text("Opciones para desarrollador"),
+                        Expanded(
+                            child: Divider(
+                                height: 1.0, endIndent: 12.0, indent: 12.0))
+                      ],
+                    ),
+                    //SizedBox(height: 20.0),
+                    //buttonAddFavorite(context: builderContext),
+                    SizedBox(height: !controller.getSaveIndicator ? 20.0 : 0.0),
+                    controller.getSaveIndicator
+                        ? Container()
+                        : buttonEditProductoOPTDeveloper(),
+                    SizedBox(height: 20.0),
+                    controller.getSaveIndicator
+                        ? Container()
+                        : buttonDeleteProductoOPTDeveloper(),
+                    SizedBox(height: 50.0),
+                  ],
+                ),
         ],
       ),
     );
@@ -395,7 +407,7 @@ class ProductEdit extends StatelessWidget {
         // usually buttons at the bottom of the dialog
         new TextButton(
           child: new Text("Cancelar"),
-          onPressed: () =>Get.back(),
+          onPressed: () => Get.back(),
         ),
         new TextButton(
           child: new Text("Borrar"),
@@ -922,5 +934,375 @@ class _SelectSubCategoriaState extends State<SelectSubCategoria> {
         );
       },
     );
+  }
+}
+
+class CreateMark extends StatefulWidget {
+  late final Marca mark;
+  CreateMark({required this.mark, Key? key}) : super(key: key);
+
+  @override
+  _CreateMarkState createState() => _CreateMarkState();
+}
+
+class _CreateMarkState extends State<CreateMark> {
+  // others controllers
+  final ControllerProductsEdit controllerProductsEdit = Get.find();
+
+  //var
+  bool newMark = false;
+  String title = 'Crear nueva marca';
+  bool load = false;
+  TextStyle textStyle = new TextStyle(fontSize: 24.0);
+  ImagePicker _picker = ImagePicker();
+  XFile xFile = XFile('');
+
+  @override
+  void initState() {
+    newMark = widget.mark.id == '';
+    title = newMark ? 'Crear nueva marca' : 'Editar';
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appbar(),
+      body: body(),
+    );
+  }
+
+  PreferredSizeWidget appbar() {
+    return AppBar(
+      title: Text(title),
+      actions: [
+        newMark
+            ? Container()
+            : IconButton(onPressed: delete, icon: Icon(Icons.delete)),
+        load
+            ? Container()
+            : IconButton(
+                icon: Icon(Icons.check),
+                onPressed: save,
+              ),
+      ],
+      bottom: load ? linearProgressBarApp() : null,
+    );
+  }
+
+  Widget body() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              xFile.path != ''
+                  ? CircleAvatar(
+                      backgroundImage: FileImage(File(xFile.path)),
+                      radius: 76,
+                    )
+                  : CachedNetworkImage(
+                      fit: BoxFit.cover,
+                      imageUrl: widget.mark.urlImage,
+                      placeholder: (context, url) => CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        radius: 75.0,
+                      ),
+                      imageBuilder: (context, image) => CircleAvatar(
+                        backgroundImage: image,
+                        radius: 75.0,
+                      ),
+                      errorWidget: (context, url, error) => CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        radius: 75.0,
+                      ),
+                    ),
+              load
+                  ? Container()
+                  : TextButton(
+                      onPressed: getLoadImageMark,
+                      child: Text("Cambiar imagen")),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextField(
+            enabled: !load,
+            controller: new TextEditingController(text: widget.mark.name),
+            onChanged: (value) => widget.mark.name = value,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: "Nombre de la marca"),
+            style: textStyle,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextField(
+            enabled: !load,
+            controller:
+                new TextEditingController(text: widget.mark.description),
+            onChanged: (value) => widget.mark.description = value,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Descripción (opcional)"),
+            style: textStyle,
+          ),
+        ),
+      ],
+    );
+  }
+
+  //  MARK CREATE
+  void getLoadImageMark() {
+    _picker
+        .pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 720.0,
+      maxHeight: 720.0,
+      imageQuality: 55,
+    )
+        .then((value) {
+      setState(() => xFile = value!);
+    });
+  }
+
+  void delete() async {
+    setState(() {
+      load = true;
+      title = 'Eliminando...';
+    });
+
+    // delele archive storage
+    if (widget.mark.id != '') {
+      await FirebaseStorage.instance
+          .ref()
+          .child("APP")
+          .child("ARG")
+          .child("MARCAS")
+          .child(widget.mark.id)
+          .delete()
+          .catchError((_) => null);
+      // delete document firestore
+      await Database.refFirestoreMark()
+          .doc(widget.mark.id)
+          .delete()
+          .then((value) {
+        // eliminar el objeto de la lista manualmente para evitar hacer una consulta innecesaria
+        controllerProductsEdit.getMarks.remove(widget.mark);
+        Get.back();
+      });
+    }
+  }
+
+  void save() async {
+    setState(() {
+      load = true;
+      title = newMark ? 'Guardando...' : 'Actualizando...';
+    });
+    if (widget.mark.id == '')
+      widget.mark.id = new DateTime.now().millisecondsSinceEpoch.toString();
+    if (widget.mark.name != '') {
+      // image save
+      // Si el "path" es distinto '' procede a guardar la imagen en la base de dato de almacenamiento
+      if (xFile.path != '') {
+        Reference ref = FirebaseStorage.instance
+            .ref()
+            .child("APP")
+            .child("ARG")
+            .child("MARCAS")
+            .child(widget.mark.id);
+        // referencia de la imagen
+        UploadTask uploadTask = ref.putFile(File(xFile.path));
+        // cargamos la imagen a storage
+        await uploadTask;
+        // obtenemos la url de la imagen guardada
+        await ref
+            .getDownloadURL()
+            .then((value) async {
+              // set
+              widget.mark.urlImage = value;
+              // mark save
+              await Database.refFirestoreMark()
+                  .doc()
+                  .set(widget.mark.toJson())
+                  .then((value) {
+                controllerProductsEdit.setMarkSelected = widget.mark;
+                // agregar el obj manualmente para evitar consulta a la db  innecesaria
+                controllerProductsEdit.getMarks.add(widget.mark);
+                Get.back();
+              });
+            })
+            .onError((error, stackTrace) {})
+            .catchError((_) {});
+      } else {
+        Get.snackbar('', 'Carga una imagen');
+      }
+    } else {
+      Get.snackbar('', 'Debes escribir un nombre de la marca');
+    }
+  }
+}
+
+class SelectMark extends StatefulWidget {
+  SelectMark({Key? key}) : super(key: key);
+
+  @override
+  _SelectMarkState createState() => _SelectMarkState();
+}
+
+class _SelectMarkState extends State<SelectMark> {
+  // var
+  ControllerProductsEdit controllerProductsEdit = Get.find();
+  List<Marca> list = [];
+
+  @override
+  void initState() {
+    loadMarks();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widgetView();
+  }
+
+  Widget widgetView() {
+    return ListView.builder(
+      padding: EdgeInsets.symmetric(vertical: 15.0),
+      shrinkWrap: true,
+      itemCount: list.length,
+      itemBuilder: (BuildContext context, int index) {
+        Marca marcaSelect = list[index];
+        if (index == 0) {
+          return Column(
+            children: [
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(bottom: 12, left: 12, right: 12),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child:
+                                Text('Marcas', style: TextStyle(fontSize: 18))),
+                        IconButton(
+                            onPressed: () {
+                              Get.back();
+                              Get.to(() => CreateMark(
+                                  mark: Marca(
+                                      timestampUpdate: Timestamp.now(),
+                                      timestampCreacion: Timestamp.now())));
+                            },
+                            icon: Icon(Icons.add)),
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: () {
+                            Get.back();
+                            showSearch(
+                              context: context,
+                              delegate: SearchPage<Marca>(
+                                items: list,
+                                searchLabel: 'Buscar marca',
+                                suggestion: Center(
+                                  child: Text('ej. Miller'),
+                                ),
+                                failure: Center(
+                                  child: Text('No se encontro :('),
+                                ),
+                                filter: (product) => [
+                                  product.name,
+                                  product.description,
+                                ],
+                                builder: (mark) => listTile(marcaSelect: mark),
+                              ),
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              listTile(marcaSelect: marcaSelect),
+              Divider(endIndent: 12.0, indent: 12.0),
+            ],
+          );
+        }
+        return Column(
+          children: <Widget>[
+            listTile(marcaSelect: marcaSelect),
+            Divider(endIndent: 12.0, indent: 12.0),
+          ],
+        );
+      },
+    );
+  }
+
+  // WIDGETS COMPONENT
+  Widget listTile({required Marca marcaSelect}) {
+    return ListTile(
+      leading: viewCircleImage(
+          texto: marcaSelect.name, url: marcaSelect.urlImage, size: 50.0),
+      dense: true,
+      title: Row(
+        children: <Widget>[
+          marcaSelect.verified == true
+              ? Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: new Image.asset('assets/icon_verificado.png',
+                      width: 16.0, height: 16.0))
+              : new Container(),
+          Expanded(
+            child: Text(marcaSelect.name,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Theme.of(context).textTheme.bodyText1!.color)),
+          ),
+        ],
+      ),
+      onTap: () {
+        controllerProductsEdit.setMarkSelected = marcaSelect;
+        Get.back();
+      },
+      onLongPress: () {
+        Get.to(() => CreateMark(mark: marcaSelect));
+      },
+    );
+  }
+
+  // functions
+  loadMarks() async {
+    if (controllerProductsEdit.getMarks.length == 0) {
+      await Database.readListMarksFuture().then((value) {
+        setState(() {
+          value.docs.forEach((element) {
+            Marca mark = Marca.fromMap(element.data());
+            mark.id = element.id;
+            list.add(mark);
+          });
+          controllerProductsEdit.setMarks = list;
+        });
+      });
+    } else {
+      // datos ya descargados
+      list = controllerProductsEdit.getMarks;
+      setState(() => list = controllerProductsEdit.getMarks);
+    }
   }
 }
